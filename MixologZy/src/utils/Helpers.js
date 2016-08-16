5034413db3eaea2337bc3490cc32888f49762f28
Tiffany Lo <tiffany.m.moore.nyc@gmail.com>
2016-08-16 00:10:44 -0400

    changes

--- MixologZy/src/utils/Helpers.js
+++ MixologZy/src/utils/Helpers.js
@@ -11,9 +11,9 @@ export default {
 
     console.log('drink name ', drinkNames);
     // when hosted online, uncomment this back
-    // return fetch(`https://addb.absolutdrinks.com/quickSearch/drinks/${drinkNames}/?apiKey=477a7635dcb248d591a7abdaf035ee32`, fetchSettings).then((response) => {
+    return fetch(`https://addb.absolutdrinks.com/quickSearch/drinks/${drinkNames}/?apiKey=477a7635dcb248d591a7abdaf035ee32`, fetchSettings).then((response) => {
     //for desktop leave uncommented
-    return fetch(`http://localhost:3000/${drinkNames}.json`, fetchSettings).then((response) => {
+    // return fetch(`http://localhost:3000/${drinkNames}.json`, fetchSettings).then((response) => {
         return response.json();
     }).catch((err) => {
         console.log(err);
